a73c138d39b29db475a33c1d31d344d95e370e47
mohsin <mohsinxx365@users.noreply.github.com>
2020-04-13 08:41:55 -0700

    Initial commit

---
+++ client/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const App = () => {
+    return (
+        <div>
+            <h1>Test</h1>
+        </div>
+    )
+}
+
+export default App
---
+++ client/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+//@ts-ignore
+import App from "./App.tsx";
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById("root")
+);
