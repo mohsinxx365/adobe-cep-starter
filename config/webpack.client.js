61db876e2aebe7a8138cb46ddc7379bb0350ed84
mohsin <mohsinxx365@users.noreply.github.com>
2020-04-13 19:59:32 -0700

    Webpack dev server works!!!!

--- config/webpack.client.js
+++ config/webpack.client.js
@@ -1,21 +1,96 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const pluginConfig = require('../pluginrc');
-const buildFolder = path.join(pluginConfig.destinationFolder, pluginConfig.extensionBundleId)
+const buildFolder = path.join(pluginConfig.destinationFolder, pluginConfig.extensionBundleId);
 const clientOutput = path.resolve(buildFolder, 'client');
 const clientEntry = path.resolve(__dirname, '../client/index.tsx');
-const htmlTemplate = path.join(__dirname, 'templates/index.template.html')
+const htmlTemplate = path.join(__dirname, 'templates/index.template.html');
 
-
-module.exports({
-    entry: clientEntry,
-    target: 'web',
-    module: {
-        rules: [
-            {
-
-            }
-        ]
-    }
-})
+module.exports = ({
+	entry: clientEntry,
+	target: 'web',
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx|tsx|ts)$/,
+				exclude: /node_modules/,
+				use: [
+					'cache-loader',
+					{
+						loader: 'babel-loader',
+						options: {
+							presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
+							plugins: [
+								[
+									'@babel/plugin-proposal-decorators',
+									{
+										legacy: true
+									}
+								],
+								'@babel/plugin-transform-object-assign',
+								'@babel/plugin-proposal-object-rest-spread',
+								'@babel/plugin-proposal-class-properties',
+								'babel-plugin-styled-components'
+							]
+						}
+					}
+				]
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|svg)$/,
+				loader: 'file-loader',
+				options: {
+					name: 'fonts/[name].[ext]'
+				}
+			},
+			{
+				test: /\.css$/,
+				use: [MiniCssExtractPlugin.loader, 'css-loader']
+			},
+			{
+				test: /\.module\.s(a|c)ss$/,
+				loader: [
+					MiniCssExtractPlugin.loader,
+					{
+						loader: 'css-loader',
+						options: {
+							modules: true
+						}
+					},
+					'sass-loader'
+				]
+			},
+			{
+				test: /\.s(a|c)ss$/,
+				exclude: /\.module.(s(a|c)ss)$/,
+				loader: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+			}
+		]
+	},
+	resolve: {
+		extensions: ['*', '.js', '.jsx', 'tsx', 'ts', '.scss']
+	},
+	output: {
+		path: clientOutput,
+		publicPath: '',
+		filename: 'bundle.js'
+	},
+	devtool: 'source-map',
+	devServer: {
+		contentBase: clientOutput
+	},
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: '[name].css',
+			chunkFilename: '[id].css'
+		}),
+		new HtmlWebpackPlugin({
+			template: htmlTemplate,
+			filename: 'index.html',
+			inject: 'body',
+			title: 'HTML Webpack Plugin',
+			bar: 'bar'
+		})
+	]
+});
---
+++ package-scripts.js
@@ -0,0 +1,6 @@
+
+module.exports = {
+    scripts: {
+        start: "webpack-dev-server --config ./config/webpack.client.js --env.target=web --mode development --open",
+    }
+};
