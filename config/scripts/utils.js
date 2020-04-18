adc9c8a42dc9366c870889bf1b12e76d3e1385ae
mohsin <mohsinxx365@users.noreply.github.com>
2020-04-18 09:32:45 -0700

    Some Changes !!!

--- config/scripts/utils.js
+++ config/scripts/utils.js
@@ -1,35 +1,36 @@
 const chalk = require('chalk');
 
 const log = () => {
-    console.log(val)
-}
+	console.log(val);
+};
 
 const log_error = (val) => {
-    log_progress(val, 'red');
-}
+	log_progress(val, 'red');
+};
 
 const log_progress = (val, color) => {
-    if (color) {
-        console.log(chalk[color](val))
-    } else {
-        console.log(chalk.hex("ffff7e")(val));
-    }
-}
+	if (color) {
+		console.log(chalk[color](val));
+	} else {
+		console.log(chalk.hex('ffff7e')(val));
+	}
+};
 
 const resolveWindows = () => {
-    return process.platform.startsWith('win');
-}
+	return process.platform.startsWith('win');
+};
 
 const resolveEnv = () => {
-    let env = 'development';
-    let args = process.argv;
+	let env = 'development';
+	var script = process.argv[2];
 
-    if (args.lenght >= 3) {
-        env = args[3];
-    }
-    return env
-}
+	return script.length >= 1 ? script : env;
+};
 
 module.exports = {
-    log, log_error, log_progress, resolveWindows, resolveEnv
-}
+	log,
+	log_error,
+	log_progress,
+	resolveWindows,
+	resolveEnv
+};
